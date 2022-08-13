24e81ecab2b8638ddb70d22e6f436dd73ff2ed2f
KING_CHAMI <110818783+KINGCHAMI@users.noreply.github.com>
2022-08-13 08:28:30 +0530

    Create Details.js

---
+++ Details.js
@@ -0,0 +1,13 @@
+/*
+KING_CHAMI Whatsapp Bot
+
+
+
+Coded By KING_CHAMI
+*/
+module.export = {
+	name: 'KING_CHAMI Whatsapp Bot',
+	version: require('./package.json').version,
+	branch: 'master',
+	author: require('./package.json').author
+}
